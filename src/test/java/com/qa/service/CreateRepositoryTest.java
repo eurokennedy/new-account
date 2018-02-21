7fdf5670a67fc92b4e59bac7e2eaeb94cfbd99a6
eurokennedy <eurokennedy@gmail.com>
2018-02-21 16:13:50 +0000

    Working model

---
+++ src/test/java/com/qa/service/CreateRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.qa.service;
+
+import static org.junit.Assert.*;
+
+import org.mockito.Mock;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CreateRepositoryTest {
+	
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void getAllAccountsTest() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void findAnAccountTest(){
+		Mockito.verify(movieService).createMovie("test");		
+	}
+	@Test
+	public void createAnAccountTest(){
+		
+	}
+	@Test
+	public void updateAnAccountTest() {
+		
+	}
+	@Test
+	public void deleteAccountTest(){
+		
+	}
+}	
